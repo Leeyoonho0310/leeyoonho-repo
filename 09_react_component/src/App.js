3ed6c18204bbfabfb46c003d06ad9ede7eaa355c
leeyoonho0310 <dkfmagustlf@gmail.com>
2024-04-03 16:58:53 +0900

    study : create react app

---
+++ 09_react_component/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import ClassComponent from './components/ClassComponent';
+import FunctionComponent from './components/FunctionComponent';
+
+function App() {
+  return (
+    <div className="App">
+      {/* 불러와 사용하는 방법 */}
+      <ClassComponent />
+      {/* 불러와 사용하는 방법2 */}
+      <ClassComponent></ClassComponent>
+      {/* 함수형 컴포넌트 */}
+      <FunctionComponent />
+    </div>
+  );
+}
+
+export default App;
