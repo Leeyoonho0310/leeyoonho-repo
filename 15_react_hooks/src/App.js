1b9c00f942c705b14a9597311e74281fabd634fb
leeyoonho0310 <dkfmagustlf@gmail.com>
2024-04-16 23:43:57 +0900

    study : ref, lifecycle, hooks

---
+++ 15_react_hooks/src/App.js
@@ -0,0 +1,19 @@
+import UseCallback1 from "./componets/UseCallback1";
+import UseCallback2 from "./componets/UseCallback2";
+import UseReducer from "./componets/UseReducer";
+import CustomHook from "./componets/CustomHook";
+import UseMemo from "./componets/UseMemo";
+
+function App() {
+  return (
+    <div className="App">
+      <UseCallback1 />
+      <UseCallback2 />
+      <UseReducer />
+      <CustomHook />
+      <UseMemo />
+    </div>
+  );
+}
+
+export default App;
