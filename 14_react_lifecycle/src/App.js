b7eda7054d870a37640379020791622a42e9a808
leeyoonho0310 <dkfmagustlf@gmail.com>
2024-04-16 23:44:40 +0900

    study : ref, lifecycle, hooks

---
+++ 14_react_lifecycle/src/App.js
@@ -0,0 +1,13 @@
+import ClassLifeCycle from "./components/ClassLifeCycle";
+import FuncLifeCycle from "./components/FuncLifeCycle";
+
+function App() {
+  return (
+    <div className="App">
+      <ClassLifeCycle />
+      <FuncLifeCycle />
+    </div>
+  );
+}
+
+export default App;
